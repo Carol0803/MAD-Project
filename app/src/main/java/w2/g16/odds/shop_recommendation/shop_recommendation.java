2b417e7a53502550621757e9df60b262175fb1a0
Wong Kak Lok <carolwong.0803@gmail.com>
2022-11-21 14:14:09 +0800

    add packages

---
+++ app/src/main/java/w2/g16/odds/shop_recommendation/shop_recommendation.java
@@ -0,0 +1,16 @@
+package w2.g16.odds.shop_recommendation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import w2.g16.odds.R;
+
+public class shop_recommendation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_recommendation);
+    }
+}
---
+++ app/src/main/java/w2/g16/odds/user_mgt/login.java
@@ -0,0 +1,16 @@
+package w2.g16.odds.user_mgt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import w2.g16.odds.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
