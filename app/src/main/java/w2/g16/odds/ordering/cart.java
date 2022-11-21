250c4cad7e3f5f9969e39b937d96bad03dd8fe9d
Wong Kak Lok <carolwong.0803@gmail.com>
2022-11-21 14:14:09 +0800

    add packages

---
+++ app/src/main/java/w2/g16/odds/ordering/cart.java
@@ -0,0 +1,16 @@
+package w2.g16.odds.ordering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import w2.g16.odds.R;
+
+public class cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
