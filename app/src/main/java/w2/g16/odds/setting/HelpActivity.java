2c235d9ed89e0bcfb55e59c24f4b877430a20aab
Haziq Hapiz <zyqq.dev@gmail.com>
2023-01-22 00:34:33 +0800

    add help button in profile and add fragments

--- app/src/main/java/w2/g16/odds/setting/HelpActivity.java
+++ app/src/main/java/w2/g16/odds/setting/HelpActivity.java
@@ -2,13 +2,19 @@ package w2.g16.odds.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import w2.g16.odds.R;
 
 public class HelpActivity extends AppCompatActivity {
+
+    Button reportFragmentBtn, aboutFragmentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,29 @@ public class HelpActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        reportFragmentBtn = findViewById(R.id.btnReport);
+        aboutFragmentBtn = findViewById(R.id.btnAbout);
+
+        reportFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new ReportFragment());
+            }
+        });
+
+        aboutFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new AboutFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.commit();
     }
 }
